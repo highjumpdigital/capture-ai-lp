4594852106f190eb33f2df5e4381cb4a17c0a3ed
Irtaza malik <127732089+develeper-devexcelit@users.noreply.github.com>
2024-12-26 11:25:59 +0500

    Refactor PlanCard and PlanDetails components to improve hover effects and styling consistency
    - Updated hover styles in both components to use larger padding and margin for better visual impact.
    - Enhanced background colors and text styles to maintain a cohesive design across the application.
    
    These changes contribute to a more user-friendly interface and improved engagement.

--- src/app/Components/PlanDetails.tsx
+++ src/app/Components/PlanDetails.tsx
@@ -14,8 +14,8 @@ export const PlanDetails: React.FC<PlanDetailsProps> = ({ hoveredIndex, onHoverI
           key={index}
           className={`font-bold Inter text-[14px] leading-[21px] cursor-pointer transition-all duration-200 ease-in-out ${
             hoveredIndex === index 
-              ? "bg-[#FFFFF9] text-[#633E34] -mx-2 px-2 py-1 rounded-md" 
-              : "-mx-2 px-2 py-1"
+              ? "bg-[#FFFFF9] text-[#633E34] -mx-5 px-5 py-1" 
+              : "-mx-5 px-5 py-1"
           }`}
           onMouseEnter={() => onHoverIndex?.(index)}
           onMouseLeave={() => onHoverIndex?.(null)}
