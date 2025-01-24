4c726433043e5a64f7e1a55658dd92286cc5abcc
Irtaza malik <127732089+develeper-devexcelit@users.noreply.github.com>
2025-01-24 19:33:50 +0500

    Refactor PaymentSol component to fetch plans data from API and enhance plan details rendering. Updated plan card styles and improved loading/error handling. Adjusted constants for better UI consistency.

--- src/app/_component/PlanDetails.tsx
+++ src/app/_component/PlanDetails.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import { constants } from '../_common/constants';
+
+interface Feature {
+  text: string;
+}
 
 interface PlanDetailsProps {
   hoveredIndex?: number | null;
   onHoverIndex?: (index: number | null) => void;
+  features: Feature[];
 }
 
-export const PlanDetails: React.FC<PlanDetailsProps> = ({ hoveredIndex, onHoverIndex }) => {
+export const PlanDetails: React.FC<PlanDetailsProps> = ({ hoveredIndex, onHoverIndex, features }) => {
   return (
     <div className="flex flex-col gap-[19px] max-w-[306px] w-[306] p-3 sm:p-5 border bg-[#FFFFFF80] border-[#00000033] plandetailshadow rounded-[5px] mt-[60px] sm:mt-[109px]">
-      {constants.planDetailsData.map((item, index) => (
+      {features.map((item, index) => (
         <div
           key={index}
           className={`font-bold Inter text-[12px] sm:text-[14px] leading-[18px] sm:leading-[21px] cursor-pointer transition-all duration-200 ease-in-out ${
